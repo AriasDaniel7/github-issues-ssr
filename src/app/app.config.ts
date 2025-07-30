6234c9cacd1338617a504b89e12961a1a1308634
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-07-30 17:40:55 -0500

    delete devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,6 +25,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
-    provideTanStackQuery(new QueryClient(), withDevtools()),
+    provideTanStackQuery(new QueryClient()),
   ],
 };
