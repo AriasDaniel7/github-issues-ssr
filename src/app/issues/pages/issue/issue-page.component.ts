9dd489c3624cbe4ceb244eb25b20a4b3b9ed8d35
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-07-30 20:27:29 -0500

    meta tags

--- src/app/issues/pages/issue/issue-page.component.ts
+++ src/app/issues/pages/issue/issue-page.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+} from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IssueService } from '@issues/services/issue.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, tap } from 'rxjs';
 import { IssueDetailItemComponent } from '@issues/components/issue-detail-item/issue-detail-item.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'issue-page',
@@ -14,6 +20,9 @@ import { IssueDetailItemComponent } from '@issues/components/issue-detail-item/i
 })
 export default class IssuePageComponent {
   private issueService = inject(IssueService);
+  private title = inject(Title);
+  private meta = inject(Meta);
+
   route = inject(ActivatedRoute);
 
   issueNumber = toSignal(
@@ -27,4 +36,37 @@ export default class IssuePageComponent {
 
   issueCommentsQuery = this.issueService.issueCommentsQuery;
   issueQuery = this.issueService.issueQuery;
+
+  metaTagsEffect = effect(() => {
+    const issue = this.issueQuery.data();
+
+    if (issue) {
+      this.title.setTitle(`Issue #${issue.number}`);
+
+      this.meta.updateTag({
+        name: 'description',
+        content: `Details of issue #${issue.number} - ${issue.title}`,
+      });
+
+      this.meta.updateTag({
+        name: 'og:title',
+        content: `Issue #${issue.number} - ${issue.title}`,
+      });
+
+      this.meta.updateTag({
+        name: 'og:description',
+        content: `Details of issue #${issue.number} - ${issue.title}`,
+      });
+
+      this.meta.updateTag({
+        name: 'keywords',
+        content: `issue, ${issue.number}, ${issue.title}`,
+      });
+
+      this.meta.updateTag({
+        name: 'og:image',
+        content: issue.user.avatar_url,
+      });
+    }
+  });
 }
